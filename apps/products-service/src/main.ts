15bb9fb5485e6babf37de2eea646e3cb933290ec
omarbougarne <omarbougarne98@gmail.com>
2026-07-14 19:54:28 +0100

    microservice up and running

--- apps/products-service/src/main.ts
+++ apps/products-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: 4002,
+    }
+  });
+  await app.listen();
+  console.log("Orders service up on 4002");
 }
 bootstrap();
