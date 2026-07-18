956c70ce8d57b22bcd8feabeacc1daf3c1249895
omarbougarne <omarbougarne98@gmail.com>
2026-07-18 19:36:06 +0100

    Api gateway

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MICROSERVICES_CLIENT } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+      name: MICROSERVICES_CLIENT.USERS_SERVICE,
+      transport: Transport.TCP,
+      options: {
+        port: 4003,
+      },
+    },
+      {
+      name: MICROSERVICES_CLIENT.PRODUCTS_SERVICE,
+      transport: Transport.TCP,
+      options: {
+        port: 4002
+      }
+    },
+    {
+      name: MICROSERVICES_CLIENT.ORDERS_SERVICE,
+      transport: Transport.TCP,
+      options: {
+        port: 4001
+      }
+    },
+  ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
